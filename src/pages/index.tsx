67cf1b547018f710f60488c8d694520e29eb1db6
cameron.adams <camgadams@gmail.com>
2023-09-16 16:40:28 +0200

    add logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
 import Head from "next/head";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
+    <div className="flex flex-col items-center justify-center gap-5">
+      <Image src="/logo.png" alt="Emz" width={500} height={500} />
       <div>Emz Bakery</div>
-      <div>I bake stuff</div>
-      <div>Call me if you want pasteries</div>
-      <div>🥐</div>
-    </>
+      <div>Email Emz if you want some amazing vegan pasteries 🥐</div>
+      <div>emzveganbaking@gmail.com</div>
+    </div>
   );
 }
