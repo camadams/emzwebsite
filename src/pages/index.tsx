68ccb4f5428ba89357e6201180d8d69964bd34f0
cameron.adams <camgadams@gmail.com>
2023-09-16 16:34:30 +0200

    Added gees

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,9 @@ export default function Home() {
   return (
     <>
       <div>Emz Bakery</div>
+      <div>I bake stuff</div>
+      <div>Call me if you want pasteries</div>
+      <div>🥐</div>
     </>
   );
 }
